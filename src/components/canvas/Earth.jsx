e253bfd982488bc53c50c34ff50ae7da7f2f76d5
Bhupendra Pratap Singh <bhupendraprataps997@gmail.com>
2025-01-23 09:58:25 -0800

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -45,7 +45,7 @@ const Earth = () => {
       <div className="mt-10">
         <a 
           href="" 
-          download="My_Resume" 
+{/*           download="My_Resume"  */}
           className="text-violet-500 hover:text-gray-500 text-3xl font-bold "
         >
           Download My Resume
