a7a8bf08d52217ca79c7f002a34cf28f303a3b2a
Bhupendra Pratap Singh <bhupendraprataps997@gmail.com>
2025-01-27 08:51:29 -0800

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -45,7 +45,7 @@ const Earth = () => {
       <div className="mt-10">
         <a 
           href="" 
-{/*           download="My_Resume"  */}
+{/*           download="https://fuchsia-therine-69.tiiny.site"  */}
           className="text-violet-500 hover:text-gray-500 text-3xl font-bold "
         >
           Download My Resume
